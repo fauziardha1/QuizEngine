e288fceba6267d4925fee1523c61949b0fdd6d89
fauziardha1 <fauziardasaputra@gmail.com>
2024-10-12 20:51:49 +0700

    add class Game to hold flow, add implementation for startgame that will return game object, implement scoring function using reduce for calculation.

--- QuizEngine/Game.swift
+++ QuizEngine/Game.swift
@@ -7,6 +7,23 @@
 
 import Foundation
 
-public func startGame<Question: Hashable, Answer, R: Router>(questions: [Question], router: R, correctAnswer: [Question: Answer]) where R.Question == Question, R.Answer == Answer {
+public class Game<Question: Hashable, Answer, R: Router> where R.Question == Question, R.Answer == Answer {
+    let flow: Flow<Question, Answer, R>
     
+    init(flow: Flow<Question,Answer, R> ) {
+        self.flow = flow
+    }
+    
+}
+
+public func startGame<Question: Hashable, Answer:Equatable, R: Router>(questions: [Question], router: R, correctAnswer: [Question: Answer]) -> Game<Question,Answer,R> where R.Question == Question, R.Answer == Answer {
+    let flow = Flow(questions: questions, router: router, scoring: {scoring($0, correctAnswer: correctAnswer)} )
+    flow.start()
+    return Game(flow: flow)
+}
+
+public func scoring<Question: Hashable, Answer: Equatable>(_ answer: [Question: Answer], correctAnswer: [Question: Answer]) -> Int {
+    return answer.reduce(0) { (score, tuple) in
+        return score + (correctAnswer[tuple.key] == tuple.value ? 1 : 0)
+    }
 }
