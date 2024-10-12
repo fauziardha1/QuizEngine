26d305f0b94cd43be8a28543819a0c944f581c35
fauziardha1 <fauziardasaputra@gmail.com>
2024-10-12 20:52:48 +0700

    add test for one correct answer, zero correct answer, and all two correct answer from two question, add setup to hold initiation of game, and remove duplication.

--- QuizEngineTests/GameTest.swift
+++ QuizEngineTests/GameTest.swift
@@ -10,14 +10,32 @@ import XCTest
 @testable import QuizEngine
 
 class GameTest: XCTestCase {
+    var game: Game<String, String, RouterSpy>?
+    let router = RouterSpy()
     
-    func test_startGame_oneOutOfTwoCorrect_score1() {
-        let router = RouterSpy()
-        startGame(questions: ["Q1","Q2"], router: router, correctAnswer: ["Q1": "A1", "Q2": "A2"])
+    override func setUp() {
+        super.setUp()
+        game = startGame(questions: ["Q1","Q2"], router: router, correctAnswer: ["Q1": "A1", "Q2": "A2"])
+    }
+    
+    func test_startGame_answerZeroOutOfTwoCorrect_scoreZero() {
+        router.answerCallback("Wrong")
+        router.answerCallback("wrong")
         
+        XCTAssertEqual(router.routedResult?.score, 0)
+    }
+    
+    func test_startGame_answerOneOutOfTwoCorrect_scoreOne() {
         router.answerCallback("A1")
         router.answerCallback("wrong")
         
         XCTAssertEqual(router.routedResult?.score, 1)
+    }
+    
+    func test_startGame_asnwerTwoOutOfTwoCorrect_scoreTwo() {
+        router.answerCallback("A1")
+        router.answerCallback("A2")
+        
+        XCTAssertEqual(router.routedResult?.score, 2)
     }
 }
